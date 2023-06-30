bd44df29c7b239af606641f26eec70e1510201f6
Justin Toft <justintoft10@gmail.com>
2023-06-30 13:57:50 -0400

    First Commit (Server Purge Code)

---
+++ README.md
@@ -0,0 +1,4 @@
+# This repository contains scripts pertaining to the uWaterloo Science Fiction and Fantasy Club
+
+## Server Purge
+A Python Discord Bot in order to purge all Discord server members with not roles 
---
+++ Server Purge/purge-users.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+# This command will kick all users with no roles
+# Run with .kicknorole
+# Admin priveleges are required
+@bot.command()
+@has_permissions(administrator=True)
+async def kicknorole(ctx: commands.Context):
+    members = ctx.guild.members
+        
+    for member in members:
+        if len(member.roles) == 1:
+            await member.kick()
+            print(member.name)
+    await ctx.reply('Done!')
+
+bot.run('Paste token here')
